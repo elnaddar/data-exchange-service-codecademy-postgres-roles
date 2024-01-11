be3c889c15f8bc0785145bf6ae11eb7db032cd31
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-01-11 16:07:55 +0200

    getting names of all users in the database.

--- script.sql
+++ script.sql
@@ -2,3 +2,7 @@
 SELECT rolname
 FROM "pg_catalog"."pg_roles"
 WHERE rolsuper = TRUE;
+
+-- What are the names of the other users in the database? What permissions do these roles have (e.g. rolcreaterole, rolcanlogin, rolcreatedb, etc.)?
+SELECT *
+FROM "pg_catalog"."pg_roles";
