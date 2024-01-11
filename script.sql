796f4fff7e5b36ec66f5137f7c9f8cb74f071aaa
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-01-11 16:10:00 +0200

    selecting current role and schema

--- script.sql
+++ script.sql
@@ -6,3 +6,7 @@ WHERE rolsuper = TRUE;
 -- What are the names of the other users in the database? What permissions do these roles have (e.g. rolcreaterole, rolcanlogin, rolcreatedb, etc.)?
 SELECT *
 FROM "pg_catalog"."pg_roles";
+
+-- Now that you have the name of the superuser, check the name of the role you’re currently using. Is this role the superuser?
+SELECT CURRENT_ROLE;
+SELECT CURRENT_SCHEMA;
